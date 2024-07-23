7fb004341a3bbbc54653ab8e8365ac465a3e0a7e
Andy Pang <chunkhai96@gmail.com>
2024-07-23 23:56:54 +0800

    Create navbar component

---
+++ components/navbar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+interface NavbarItem {
+  title: string,
+  icon: string,
+  link: string
+}
+const navbarItem: Array<NavbarItem> = [
+  { title: 'Home', icon: 'home', link: '/' },
+  { title: 'Contact', icon: 'contact', link: '/contact' }
+]
+</script>
+<template>
+  <div class="rounded-full h-15 flex py-4">
+    <div v-for="item in navbarItem"
+      class="py-2 px-4 rounded-full hover:cursor-pointer hover:bg-gray-200">
+      {{ item.title }}
+    </div>
+  </div>
+</template>
