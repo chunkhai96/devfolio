5bf2c0ed5091908202165638c60fd0eb6d2edb8a
Andy Pang <chunkhai96@gmail.com>
2024-07-24 23:50:54 +0800

    Add card component

---
+++ components/card.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+<template>
+  <div class="w-full bg-slate-800/50 backdrop-blur-sm rounded-3xl border border-slate-700">
+    <slot name="imgHeader"></slot>
+    <div class="flex flex-col gap-4 p-8">
+      <slot name="header"></slot>
+      <slot name="content"></slot>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
