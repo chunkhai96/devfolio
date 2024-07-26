5cb08655d474690cd68ab9fb3c3d3e80430eaa34
Andy Pang <chunkhai96@gmail.com>
2024-07-26 23:45:49 +0800

    Create TechStackContainer component

---
+++ components/TechStackContainer.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+
+const props = defineProps({
+  icon: {
+    type: Object as () => IconDefinition,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+})
+</script>
+<template>
+  <div class="flex flex-col gap-4 items-center text-slate-400 transition duration-300 hover:text-indigo-300 hover:scale-110">
+    <fa class="text-5xl"
+      :icon="props.icon" />
+    <span class="text-xs">
+      {{ props.text }}
+    </span>
+  </div>
+</template>
