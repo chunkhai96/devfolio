51428cbb26cdc5dcd3c45c5d1f8f2ffee908da76
Andy Pang <chunkhai96@gmail.com>
2024-07-26 23:46:06 +0800

    Install font awesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: ['@vesp/nuxt-fontawesome'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  fontawesome: {
+    component: 'fa',
+    suffix: false,
+    icons: {
+      brands: ['square-js']
+    }
+  }
 })
