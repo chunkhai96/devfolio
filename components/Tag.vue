3df70e90dfef7c3251f5db60d1ea39b7debe4cba
Andy Pang <chunkhai96@gmail.com>
2024-07-27 22:26:49 +0800

    Create tag component

---
+++ components/Tag.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    default: ''
+  }
+})
+const { text, size } = toRefs(props)
+</script>
+<template>
+  <div class=" flex justify-center items-center w-full bg-slate-800/50 backdrop-blur-sm rounded-3xl border border-slate-700 p-2">
+    <span :class="size">
+      {{ text }}
+    </span>
+  </div>
+</template>
