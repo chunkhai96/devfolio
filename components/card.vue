fa3d85a6980fb856fabf0649da3c83e75d031419
Andy Pang <chunkhai96@gmail.com>
2024-07-30 00:27:26 +0800

    Update card component

--- components/card.vue
+++ components/card.vue
@@ -4,9 +4,9 @@
   <div class="w-full bg-slate-800/50 backdrop-blur-sm rounded-3xl border border-slate-700">
     <slot name="imgHeader"></slot>
     <div class="flex flex-col gap-2 h-full w-full p-4">
-      <slot name="header"></slot>
-      <slot name="content"></slot>
-      <slot name="footer"></slot>
+      <div><slot name="header"></slot></div>
+      <div><slot name="content"></slot></div>
+      <div><slot name="footer"></slot></div>
     </div>
   </div>
 </template>
