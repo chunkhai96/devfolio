0532c666cc3078d05f07c4261da039e570ca56e6
Andy Pang <chunkhai96@gmail.com>
2024-07-30 23:51:46 +0800

    Create IconButton component

---
+++ components/IconButton.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps({
+  icon: {
+    type: String
+  },
+})
+const { icon } = toRefs(props)
+</script>
+<template>
+  <button class="bg-slate-500 hover:bg-slate-700 text-white font-bold size-12 p-2 rounded-full">
+    <i class="text-3xl" :class="icon"></i>
+  </button>
+</template>
