095ea4140e49b800598c74d212e72f96bf71a58d
Andy Pang <chunkhai96@gmail.com>
2024-07-31 23:59:03 +0800

    Update IconButton component

--- components/IconButton.vue
+++ components/IconButton.vue
@@ -3,11 +3,24 @@ const props = defineProps({
   icon: {
     type: String
   },
+  src: {
+    type: String
+  },
+  color: {
+    type: String,
+    default: 'bg-slate-500'
+  },
+  textColor: {
+    type: String,
+    default: 'text-white'
+  }
 })
 const { icon } = toRefs(props)
 </script>
 <template>
-  <button class="bg-slate-500 hover:bg-slate-700 text-white font-bold size-12 p-2 rounded-full">
-    <i class="text-3xl" :class="icon"></i>
+  <button class="font-bold size-12 rounded-full flex justify-center items-center transition duration-300 hover:scale-110"
+    :class="[color, textColor]">
+    <i v-if="icon" class="p-2 text-2xl" :class="icon"></i>
+    <img v-else-if="src" class="text-2xl" :src="src" />
   </button>
 </template>
