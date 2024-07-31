56c709e9feb8e7a2df95bb2529e9f9024228cba3
Andy Pang <chunkhai96@gmail.com>
2024-08-01 00:41:20 +0800

    Add fontawesome to IconButton component

--- components/IconButton.vue
+++ components/IconButton.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
+import type {
+  IconDefinition
+} from '@fortawesome/free-solid-svg-icons'
+
 const props = defineProps({
-  icon: {
+  devicon: {
     type: String
   },
   src: {
     type: String
+  },
+  fontawesome: {
+    type: Object as ()=> IconDefinition
   },
   color: {
     type: String,
@@ -15,12 +22,13 @@ const props = defineProps({
     default: 'text-white'
   }
 })
-const { icon } = toRefs(props)
+const { devicon, src, fontawesome, color, textColor } = toRefs(props)
 </script>
 <template>
   <button class="font-bold size-12 rounded-full flex justify-center items-center transition duration-300 hover:scale-110"
     :class="[color, textColor]">
-    <i v-if="icon" class="p-2 text-2xl" :class="icon"></i>
+    <i v-if="devicon" class="p-2 text-2xl" :class="devicon"></i>
     <img v-else-if="src" class="text-2xl" :src="src" />
+    <fa v-else-if="fontawesome" :icon="fontawesome" class="text-2xl" />
   </button>
 </template>
