2eb53c3b866ecbc7dfb5e1aa93efa0d1d8153ded
Andy Pang <chunkhai96@gmail.com>
2024-08-03 00:37:53 +0800

    Create Button component

---
+++ components/Button.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps({
+  label:{
+    type: String,
+    required: true
+  },
+  textSizeClass: {
+    type: String,
+  },
+})
+const { label, textSizeClass } = toRefs(props)
+</script>
+<template>
+  <button class="py-2 px-4 rounded-full flex justify-center items-center border-2 border-slate-600 transition duration-300 hover:bg-slate-600">
+    <span :class="[textSizeClass]">
+      {{ label }}
+    </span>
+  </button>
+</template>
