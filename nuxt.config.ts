f5266f293ed910369a20ad9d95da6e57b3f783a7
Andy Pang <chunkhai96@gmail.com>
2024-08-04 15:27:44 +0800

    Add marquee

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css', 'devicon/devicon.min.css'],
-  modules: ['@vesp/nuxt-fontawesome'],
+  modules: ['@vesp/nuxt-fontawesome', "nuxt-marquee"],
   postcss: {
     plugins: {
       tailwindcss: {},
