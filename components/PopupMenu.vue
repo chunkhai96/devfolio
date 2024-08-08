c8d0dd34b2d09b1bfe3b5d3fda5b7a2e0e456603
Andy Pang <chunkhai96@gmail.com>
2024-08-08 23:17:37 +0800

    Fix popup menu

--- components/PopupMenu.vue
+++ components/PopupMenu.vue
@@ -10,30 +10,34 @@ const props = defineProps({
 })
 const { items } = toRefs(props)
 
+const showMenu = ref<boolean>(false)
+const dropdownMenuButton = ref<InstanceType<typeof HTMLElement>>()
 const dropdownMenu = ref<InstanceType<typeof HTMLElement>>()
 const toggleDropdown = () => {
-  console.log(dropdownMenu.value?.classList)
-  if (dropdownMenu.value?.classList.contains('hidden')) {
-    dropdownMenu.value?.classList.remove('hidden')
-    dropdownMenu.value?.focus()
+  if (!showMenu.value) {
+    showMenu.value = true
+    window.setTimeout(() => dropdownMenu.value?.focus(), 0)
   } else {
-    dropdownMenu.value?.classList.add('hidden')
+    showMenu.value = false
   }
   dropdownMenu.value?.addEventListener('focusout', (event) => {
-    dropdownMenu.value?.classList.add('hidden')
+    window.setTimeout(() => showMenu.value = false, 0)
   })
 }
 </script>
 <template>
   <div class="relative inline-block mx-2 text-left"> 
     <button class="size-12 rounded-full shadow hover:ring hover:ring-slate-400 focus:outline-none"
+      ref="dropdownMenuButton"
+      tabindex="0"
       @click="toggleDropdown">
       <fa :icon="faBars" />
     </button>
-    <div class="hidden origin-bottom absolute right-0 w-56 shadow-lg bg-slate-700/80 backdrop-blur-md z-30 rounded-3xl focus:bg-blue-600"
+    <div class="origin-bottom absolute min-w-[12rem] right-0 shadow-lg py-2 bg-slate-700/80 backdrop-blur-md z-30 rounded-3xl"
+      :class="{ 'hidden': !showMenu }"
       ref="dropdownMenu"
       id="dropdownMenuBottom"
-      tabindex="0">
+      tabindex="1">
       <NuxtLink v-for="item in items"
         class="block px-4 py-2 hover:bg-slate-700 rounded-full"
         :href="item.link">
