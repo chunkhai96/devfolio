47d707bd379573ba628344be457be3399ff893a2
Andy Pang <chunkhai96@gmail.com>
2024-08-09 16:02:03 +0800

    Update tag component

--- components/Tag.vue
+++ components/Tag.vue
@@ -13,7 +13,8 @@ const { text, size } = toRefs(props)
 </script>
 <template>
   <div class=" flex justify-center items-center w-full bg-slate-800/50 backdrop-blur-sm rounded-3xl border border-slate-700 p-2">
-    <span :class="size">
+    <span class="text-center"
+      :class="size">
       {{ text }}
     </span>
   </div>
