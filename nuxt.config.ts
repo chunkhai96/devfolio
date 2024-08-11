24176e2d5b23ce37534bd45408030287442840b1
Andy Pang <chunkhai96@gmail.com>
2024-08-11 16:20:41 +0800

    Add relevant meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css', 'devicon/devicon.min.css'],
-  modules: ['@vesp/nuxt-fontawesome', "nuxt-marquee"],
+  modules: ['@vesp/nuxt-fontawesome', "nuxt-marquee", "@nuxtjs/seo"],
   runtimeConfig:{
     public: {
       web3FormAccessKey: ''
@@ -17,9 +22,13 @@ export default defineNuxtConfig({
   },
   fontawesome: {
     component: 'fa',
-    suffix: false,
-    icons: {
-      solid: ['calendar-alt']
-    }
+    suffix: false
+  },
+  site: {
+    url: process.env.NUXT_PUBLIC_SITE_URL,
+    name: 'Andy Pang - Devfolio',
+    description: 'Welcome to the online portfolio of Andy Pang (aka Pang Chun Khai), a passionate machine learning engineer, software engineer and data scientist dedicated to creating cutting-edge solutions. Explore his projects and experiences in machine learning, AI development, and innovative technology.',
+    defaultLocale: 'en',
+    indexable: process.env.NUXT_SITE_ENV === 'production'
   }
 })
