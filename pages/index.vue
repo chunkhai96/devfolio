66793d32657ebc0ff1da28daf3ac233a032375e8
Andy Pang <chunkhai96@gmail.com>
2024-08-14 22:24:55 +0800

    Update achievements images

--- pages/index.vue
+++ pages/index.vue
@@ -128,7 +128,7 @@ const talkSectionText = {
 const talkItems = [
   {
     title: 'Competent in Job Market Through Final Year Project Management',
-    imageSrc: 'images/talk-uthm.png',
+    imageSrc: 'images/talk-uthm.webp',
     youtube: 'https://youtu.be/skoynvqo64o?si=7wzZnsVE4ziiZ4qt',
     slide: 'https://docs.google.com/presentation/d/19JZlfQRdsyLvZ91-RtQGYBnsyXRKvAlCJR03ll6JHEg/edit?usp=sharing'
   }
@@ -143,49 +143,49 @@ const achievementItems = [
     title: 'Microsoft Certified: Azure Fundamentals',
     provider: 'Microsoft',
     link: 'https://learn.microsoft.com/api/credentials/share/en-us/andypang/920020771177B6BE?sharingId=973E5703531249ED',
-    imageSrc: 'images/azure-fundamental-logo.png'
+    imageSrc: 'images/azure-fundamental-logo.webp'
   },
   {
     title: 'Natural Language Processing Specialization',
     provider: 'DeepLearning.AI',
     link: 'https://coursera.org/share/3c6e15d0f2306504f6751da1e2195356',
-    imageSrc: 'images/deep-learning-ai-logo.png'
+    imageSrc: 'images/deep-learning-ai-logo.webp'
   },
   {
     title: 'Data Analyst Nanodegree',
     provider: 'Udacity',
     link: 'https://www.udacity.com/certificate/QRRDHARF',
-    imageSrc: 'images/udacity-logo.png'
+    imageSrc: 'images/udacity-logo.webp'
   },
   {
     title: 'Programming with Google Go Specialization',
     provider: 'University of California Irvine',
     link: 'https://coursera.org/share/a0e910c581580aa9297d14ee71ee2ed6',
-    imageSrc: 'images/uci-logo.png'
+    imageSrc: 'images/uci-logo.webp'
   },
   {
     title: 'Deep Learning Specialization',
     provider: 'DeepLearning.AI',
     link: 'https://coursera.org/share/b64de4b8cecec71649b37bc147802e65',
-    imageSrc: 'images/deep-learning-ai-logo.png'
+    imageSrc: 'images/deep-learning-ai-logo.webp'
   },
   {
     title: 'Machine Learning',
     provider: 'Stanford University',
     link: 'https://coursera.org/share/a9326fa001b85ec2459909389f8fee02',
-    imageSrc: 'images/stanford-logo.png'
+    imageSrc: 'images/stanford-logo.webp'
   },
   {
     title: 'Machine Learning on Google Cloud Specialization',
     provider: 'Google Cloud',
     link: 'https://coursera.org/share/23ff1c353e4d9649f9c023e13180e7fc',
-    imageSrc: 'images/google-cloud-logo.png'
+    imageSrc: 'images/google-cloud-logo.webp'
   },
   {
     title: 'Front-End Web Development with React',
     provider: 'The Hong Kong University of Science and Technology',
     link: 'https://coursera.org/share/ec4d848c78ed50bab7f172a4e0cefe92',
-    imageSrc: 'images/hkust-logo.png'
+    imageSrc: 'images/hkust-logo.webp'
   }
 ]
 const marqueeSpeed = ref<number>(50)
