63897efbfe4d00abf6ced7962c0386c54265df1a
Andy Pang <chunkhai96@gmail.com>
2024-08-21 23:37:39 +0800

    Move profile picture

--- data/landing.ts
+++ data/landing.ts
@@ -1,6 +1,7 @@
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import type { ExternalLink } from '.'
 
+const profilePictureSrc: string = '/images/avatar.webp'
 const title: string = 'Andy Pang'
 const description: string = 'I solve problems through innovation using code.'
 const resumeSrc: string = 'https://drive.google.com/file/d/1hWBVXXyK6ykCb8c82DddwkF0XtBQGswn/view?usp=sharing'
@@ -48,6 +49,7 @@ export type {
 }
 
 export {
+  profilePictureSrc,
   title,
   description,
   resumeSrc,
